7f7fccffec924b3a5b751cff3a2cd82455d04cf1
Ashish Kanswal <ashish.kanswal@bigbasket.com>
2023-11-01 18:48:26 +0530

    update test coverage report

--- pa-server/src/database/config.ts
+++ pa-server/src/database/config.ts
@@ -5,9 +5,7 @@ const dbUser = process.env.PG_USERNAME as string || "admin"
 const dbPassword = process.env.PG_PASSWORD || "StrongPass"
 const dbHost = process.env.PG_HOST || "localhost"
 const dbPort = process.env.PG_PORT as unknown as number || 5432
-const logging = process.env.PG_LOGGING as unknown as boolean || true
 const environment = process.env.env || "dev"
-const testStorage = environment === "test" ? ":memory:" : null
 
 console.log(`Using environment: ${environment}`)
 
--- pa-server/src/database/dal/AllotmentDal.ts
+++ pa-server/src/database/dal/AllotmentDal.ts
@@ -6,52 +6,6 @@ import Floor from "../entities/Floor"
 
 export class AllotmentDal {
 
-    updateSlotWithVehicleNumber = async (slotId: number, vehicleNumber: string): Promise<number> => {
-        let updated = await ParkingSlot.update({ vehicleNumber: vehicleNumber }, {
-            where: { id: slotId }
-        })
-        if (updated.length > 0) {
-            return updated[0]
-        }
-        return -1
-    }
-
-    allotSlot = async (lotId: number, size: number, verhicleNumber: string): Promise<AllotmentResponse> => {
-        try {
-            const result = await sequelizeConnection.transaction(async (t) => {
-                //Need to use ORM here, but due to time constraint wrote a query directly
-                let slot = await sequelizeConnection.query(`
-            SELECT ps.id as "slotId", "FloorId" as "floorId", f.name as "floorName", ps.name as "slotName", f.level as "level"
-            FROM "ParkingSlots" ps 
-            JOIN "Floors" f 
-            ON ps."FloorId" = f.id 
-            AND ps."vehicleNumber" IS NULL 
-            WHERE f."ParkingLotId" = ${lotId}
-            AND ps.size >= ${size}
-            ORDER BY ps.size ASC LIMIT 1`, { transaction: t })
-
-                if (slot.length > 0 && slot[0].length > 0) {
-                    let slotResponse = slot[0][0] as AllotmentResponse
-                    if (slotResponse != null && slotResponse.slotId > 0) {
-                        let updated = await ParkingSlot.update({ vehicleNumber: verhicleNumber }, {
-                            transaction: t,
-                            where: { id: slotResponse.slotId }
-                        })
-                        if (updated.length > 0 && updated[0] > 0) {
-                            slotResponse.slot = `${slotResponse.floorId}-${slotResponse.slotId}`
-                            return slotResponse
-                        }
-                    } else {
-                        throw new ApiError(500, "All slots are full")
-                    }
-                }
-            })
-            return result
-        } catch (err) {
-            throw err
-        }
-    }
-
     updateSlotRemoveVehicleNumber = async (slotId: number, vehicleNumber: string): Promise<number> => {
         let result = await ParkingSlot.update({ vehicleNumber: vehicleNumber }, {
             where: { id: slotId },
@@ -59,7 +13,6 @@ export class AllotmentDal {
         if (result.length > 0) {
             return result[0]
         }
-        return -1
     }
 
     releaseSlotVehicleNumber = async (slotId: number): Promise<number> => {
@@ -69,7 +22,6 @@ export class AllotmentDal {
         if (result.length > 0) {
             return result[0]
         }
-        return -1
     }
     getParkingLotsByRegistrationId = async (lotId: number): Promise<AllotmentResponse[]> => {
         let res = await sequelizeConnection.query(`
@@ -83,7 +35,6 @@ export class AllotmentDal {
         if (res.length > 0) {
             return res[0] as AllotmentResponse[]
         }
-        return null
     }
 
     getSlotById = async (slotId: number): Promise<any> => {
